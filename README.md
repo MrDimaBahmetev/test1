4e02b12d30181cebad5c0bf36028fa00e70438b1
Дмитрий локал <bahmetev168@gmail.com>
2026-07-09 14:59:49 +0300

    add main module and tests

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Lesson for Git and Pyhton
+
+## Install
+
+1. Клонируйте репозиторий
+2. Создайте виртаульное
+3. Активируйте
+4. Установите зависимости (pip install -r requirements.txt)
+
+## Use
+python main.py
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+import pytest
+from main import calculate_avarage
+
+def test_calculate_avarage():
+    assert calculate_avarage([10,20,30]) == 20.0
+    assert calculate_avarage([]) == 0.0
+    assert calculate_avarage([5]) == 5.0
